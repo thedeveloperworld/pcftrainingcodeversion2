080ec5daf9f5da66700b3f9197e35d1c65c606d5
amitdatta <the.amistudy@gmail.com>
2017-07-16 12:56:44 +0530

    First commit.

--- src/main/java/com/amisoft/controller/amisoftWelcomeController.java
+++ src/main/java/com/amisoft/controller/amisoftWelcomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RefreshScope
+@EnableResourceServer
 public class amisoftWelcomeController {
 
     @Value(value = "${amisoft.service.welcome}")
